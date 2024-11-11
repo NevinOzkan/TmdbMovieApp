4114369e4c30fc35e0bd9b024a121cde44fa2ef9
Nevin Ozkan <ozkannevin73@gmail.com>
2024-11-11 16:07:46 +0300

    Created Project

---
+++ TmdbMovieApp/Scenes/Home/View/HomeVC.swift
@@ -0,0 +1,18 @@
+//
+//  HomeVC.swift
+//  TmdbMovieApp
+//
+//  Created by Nevin Özkan on 11.11.2024.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
