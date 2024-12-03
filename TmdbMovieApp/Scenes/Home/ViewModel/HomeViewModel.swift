1cc5ac36058d08ee9b06b1903f60d924b7bc9951
Nevin Ozkan <ozkannevin73@gmail.com>
2024-12-03 13:34:24 +0300

    Fifth improvement

--- TmdbMovieApp/Scenes/Home/ViewModel/HomeViewModel.swift
+++ TmdbMovieApp/Scenes/Home/ViewModel/HomeViewModel.swift
@@ -8,24 +8,29 @@
 import Foundation
 
 class HomeViewModel: MovieViewModelProtocol {
-    var currentPage: Int = 0
+    
     var delegate: (any MovieViewModelDelegate)?
     var nowPlayingMovies: [MovieModel] = []
     var upcomingMovies: [MovieModel] = []
     var service = MovieService()
+    var currentPage: Int = 1
+    var isLoading: Bool = false
     
-    func loadUpcomingMovies(page: Int) {
-        service.fetchUpcomingMovies() { [weak self] result in
-            guard let self = self else { return }
-            switch result {
-            case .success(let response):
-                self.upcomingMovies.append(contentsOf: response.results)
-                self.notify(.updateUpcomingMovies(self.upcomingMovies))
-            case .failure(let error):
-                self.delegate?.showError("Failed to load movie details: \(error.localizedDescription)")
+    func loadUpcomingMovies() {
+        service.fetchUpcomingMovies(page: currentPage) { [weak self] result in
+                guard let self = self else { return }
+                self.isLoading = false
+                
+                switch result {
+                case .success(let response):
+                    self.upcomingMovies.append(contentsOf: response.results)
+                    self.currentPage += 1
+                    self.notify(.updateUpcomingMovies(self.upcomingMovies))
+                case .failure(let error):
+                    self.delegate?.showError("Failed to load movie details: \(error.localizedDescription)")
+                }
             }
         }
-    }
     
     func loadNowPlayingMovies() {
         service.fetchNowPlayingMovies() { [weak self] result in
