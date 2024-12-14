2764eabe77965d57568d87c257f08ff98b765183
Nevin Ozkan <ozkannevin73@gmail.com>
2024-12-14 14:43:52 +0300

    Delegate move to homeviewmodel

--- TmdbMovieApp/Scenes/Home/ViewModel/HomeViewModel.swift
+++ TmdbMovieApp/Scenes/Home/ViewModel/HomeViewModel.swift
@@ -61,3 +61,24 @@ class HomeViewModel: MovieViewModelProtocol {
         delegate?.handleViewModelOutput(output)
     }
 }
+
+protocol MovieViewModelProtocol {
+    var delegate: MovieViewModelDelegate? { get set }
+    var upcomingMovies: [MovieModel] { get set }
+    var nowPlayingMovies: [MovieModel] { get set }
+    var currentPage: Int { get set }
+    var isLoading: Bool { get set }
+    var totalPages: Int { get set }
+    func loadUpcomingMovies()
+    func loadNowPlayingMovies()
+}
+
+enum MovieViewModelOutput {
+    case updateUpcomingMovies([MovieModel])
+    case updateNowPlayingMovies([MovieModel])
+}
+
+protocol MovieViewModelDelegate: AnyObject {
+    func handleViewModelOutput(_ output: MovieViewModelOutput)
+    func showError(_ message: String)
+}
